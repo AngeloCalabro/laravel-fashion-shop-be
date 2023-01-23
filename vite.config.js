637b3adaeb85fac31fbc9c2820fb57dd45cbd1af
AngeloCalabro <angelo.1991@hotmail.com>
2023-01-23 17:48:13 +0100

    icon fonts

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,7 @@ export default defineConfig({
     resolve: {
         alias: {
             '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+            '~@fortawesome': path.resolve(__dirname, 'node_modules/@fortawesome'),
             '~resources': '/resources/'
         }
     }
