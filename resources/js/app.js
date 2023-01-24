2c39f1e59f76884a5d44d8080cd0655ab60ba91e
AngeloCalabro <angelo.1991@hotmail.com>
2023-01-24 16:19:12 +0100

    image js

--- resources/js/app.js
+++ resources/js/app.js
@@ -29,3 +29,13 @@ deleteSubmitButtons.forEach((button) => {
         })
     })
 });
+
+const previewImage = document.getElementById('image');
+previewImage.addEventListener('change', (event) => {
+    var oFReader = new FileReader();
+    oFReader.readAsDataURL(previewImage.files[0]);
+
+    oFReader.onload = function (oFREvent) {
+        document.getElementById("uploadPreview").src = oFREvent.target.result;
+    };
+});
