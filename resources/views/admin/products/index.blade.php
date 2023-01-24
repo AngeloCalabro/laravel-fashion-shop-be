b5834df8bee7e5c4275ef9c0c13455b0ceb210ff
Lorenzo Castiglion <lorenzo.castiglion@hotmail.it>
2023-01-24 15:23:00 +0100

    rapper table

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -11,55 +11,57 @@
         {{ session()->get('message') }}
     </div>
     @endif
-
-    <table class="table table-striped">
-        <thead>
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">Name</th>
-            <th scope="col">Image</th>
-            <th scope="col">Description</th>
-            <th scope="col">Price</th>
-            <th scope="col">Category</th>
-            <th scope="col">Texture</th>
-            <th scope="col">Brand</th>
-            <th scope="col">Edit</th>
-            <th scope="col">Delete</th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($products as $product)
-                <tr>
-                    <th scope="row">{{$product->id}}</th>
-                    <td>
-                        <a href="{{route('admin.products.show', $product->slug)}}" title="View Product">{{$product->name}}</a>
-                    </td>
-
-                    <td><img src="{{ asset('storage/' .$product->api_featured_images) }}" alt="{{$product->name}}"></td>
-
-                    <td>{{Str::limit($product->description,50)}}</td>
-                    <td>{{$product->price}} {{$product->price_sign}}</td>
-                    <td>{{$product->type ? $product->type->name : 'Senza categoria'}}</td>
-                    <td>{{$product->texture ? $product->texture->name : 'Senza texture'}}</td>
-                    <td>{{$product->brand ? $product->brand->name : 'Senza brand'}}</td>
-                    <td>
-                        <a class="link-secondary" href="{{route('admin.products.edit', $product->slug)}}" title="Edit product">
-                            <i class="fa-solid fa-pen"></i>
-                        </a>
-                    </td>
-                    <td>
-                        <form action="{{route('admin.products.destroy', $product->slug)}}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="delete-button btn btn-danger ms-3" data-item-title="{{$product->name_product}}">
-                            <i class="fa-solid fa-trash-can"></i>
-                        </button>
-                     </form>
-                </tr>
-        @endforeach
-
-        </tbody>
-    </table>
+<div class="table-wrapper  overflow-auto">
+    
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Image</th>
+                <th scope="col">Description</th>
+                <th scope="col">Price</th>
+                <th scope="col">Category</th>
+                <th scope="col">Texture</th>
+                <th scope="col">Brand</th>
+                <th scope="col">Edit</th>
+                <th scope="col">Delete</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($products as $product)
+                    <tr>
+                        <th scope="row">{{$product->id}}</th>
+                        <td>
+                            <a href="{{route('admin.products.show', $product->slug)}}" title="View Product">{{$product->name}}</a>
+                        </td>
+    
+                        <td><img src="{{ asset('storage/' .$product->api_featured_images) }}" alt="{{$product->name}}"></td>
+    
+                        <td>{{Str::limit($product->description,50)}}</td>
+                        <td>{{$product->price}} {{$product->price_sign}}</td>
+                        <td>{{$product->type ? $product->type->name : 'Senza categoria'}}</td>
+                        <td>{{$product->texture ? $product->texture->name : 'Senza texture'}}</td>
+                        <td>{{$product->brand ? $product->brand->name : 'Senza brand'}}</td>
+                        <td>
+                            <a class="link-secondary" href="{{route('admin.products.edit', $product->slug)}}" title="Edit product">
+                                <i class="fa-solid fa-pen"></i>
+                            </a>
+                        </td>
+                        <td>
+                            <form action="{{route('admin.products.destroy', $product->slug)}}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="delete-button btn btn-danger ms-3" data-item-title="{{$product->name_product}}">
+                                <i class="fa-solid fa-trash-can"></i>
+                            </button>
+                         </form>
+                    </tr>
+            @endforeach
+    
+            </tbody>
+        </table>
+</div>
     {{-- {{ $products->links('vendor.pagination.bootstrap-5') }} --}}
     @include('partials.admin.modal-delete')
 @endsection
