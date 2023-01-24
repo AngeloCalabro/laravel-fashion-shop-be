cd08b7c6db7bb33e0aed72adca0283f6faff0b51
“BarrioTropical” <barrio>
2023-01-24 18:37:08 +0100

    Mail, Lead Controller, New Contact e Model Contact
    Signed-off-by: “BarrioTropical” <“barrio>

---
+++ app/Http/Controllers/Api/LeadController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Mail\NewContact;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Mail;
+use App\Models\Lead;
+
+
+class LeadController extends Controller
+{
+    public function store(Request $request){
+        $data = $request->all();
+
+        $validator = Validator::make($data, [
+            'name' => 'required',
+            'email' => 'required|email',
+            'message' => 'required',
+        ]);
+        if ($validator->fails()){
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()
+            ]);
+        }
+
+        $new_lead = new Lead();
+        $new_lead->fill($data);
+        $new_lead->save();
+
+        Mail::to('info@boolpress.com')->send(new NewContact($new_lead));
+        return response()->json([
+            'success' => true
+        ]);
+
+    }
+}
